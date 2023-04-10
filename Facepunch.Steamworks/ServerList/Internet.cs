c2f81e081f79d2e4f792e13096e8dd0f56543e15
anttiverse <antti@roleverse.com>
2023-04-11 02:07:15 +0300

    Filters are now stored in a static buffer in Internet server list query. This is an attempt to get rid of a very rare crash that seems to be related to the key value pair array crashing when being marshalled. One theory is that there is something wrong with GC and this change tries to prevent the array getting garbage collected. Now waiting to see if the crash still happens...
    More info:
    https://forum.unity.com/threads/il2cpp-marshalling-issue-with-an-empty-arrays-of-structs.1423185
    https://github.com/Facepunch/Facepunch.Steamworks/issues/658

--- Facepunch.Steamworks/ServerList/Internet.cs
+++ Facepunch.Steamworks/ServerList/Internet.cs
@@ -8,11 +8,22 @@ namespace Steamworks.ServerList
 {
 	public class Internet : Base
 	{
+		// There is a very rare crash in Internal.RequestInternetServerList, somehow related to filters.
+		// More info:
+		// https://forum.unity.com/threads/il2cpp-marshalling-issue-with-an-empty-arrays-of-structs.1423185
+		// https://github.com/Facepunch/Facepunch.Steamworks/issues/658
+		//
+		// Using this static reference is a hack that tries to make sure filters are not freed prematurely by garbage collection.
+		// No idea if this really helps.
+		//
+		internal static Steamworks.Data.MatchMakingKeyValuePair[] sm_filters;
+
 		internal override void LaunchQuery()
 		{
-			var filters = GetFilters();
+			// var filters = GetFilters(); // Old code before the hack workaround attempt
+			sm_filters = GetFilters(); // See comments above - making sure filters has a long lifetime (until the function called again)
 
-			request = Internal.RequestInternetServerList( AppId.Value, ref filters, (uint)filters.Length, IntPtr.Zero );
+			request = Internal.RequestInternetServerList( AppId.Value, ref sm_filters, (uint)sm_filters.Length, IntPtr.Zero );
 		}
 	}
 }
